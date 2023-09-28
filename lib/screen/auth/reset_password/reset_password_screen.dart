e9dc9cf0681ba17794f4952981e114628b29dde3
alaa baaj <the.lost.admiral@gmail.com>
2023-09-29 02:13:54 +0300

    commit

--- lib/screen/auth/reset_password/reset_password_screen.dart
+++ lib/screen/auth/reset_password/reset_password_screen.dart
@@ -2,7 +2,6 @@ import 'package:beezer_v2/screen/auth/login/login_screen.dart';
 import 'package:beezer_v2/widget/app_bar_auth.dart';
 import 'package:beezer_v2/widget/elevated_button_def.dart';
 import 'package:beezer_v2/widget/have_account.dart';
-import 'package:beezer_v2/widget/text_form_field_def.dart';
 import 'package:beezer_v2/widget/text_form_field_password_def.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
