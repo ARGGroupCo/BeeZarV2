19517d72bb8fbf4723e80ac3a9d1a4f9728ec7d4
alaa baaj <the.lost.admiral@gmail.com>
2023-10-08 00:09:58 +0300

    commit

--- lib/screen/home/page/profile_screen.dart
+++ lib/screen/home/page/profile_screen.dart
@@ -1,10 +1,10 @@
 import 'package:beezer_v2/res/font_def.dart';
 import 'package:beezer_v2/screen/home/page/profile_edit_screen.dart';
 import 'package:beezer_v2/screen/home/widget/app_bar_profile.dart';
+import 'package:beezer_v2/screen/home/widget/profile_button.dart';
 import 'package:beezer_v2/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../../res/color_manager.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -30,180 +30,25 @@ class ProfileScreen extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  InkWell(
-                    onTap: () => Get.to(const ProfileEditScreen()),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      decoration: (BoxDecoration(
-                        color: ColorManager.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 5),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(15),
-                      )),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("تعديل الصفحة الشخصية"),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(Icons.edit),
-                          ],
-                        ),
-                      ),
-                    ),
+                  ProfileButton(
+                    press: () => Get.to(const ProfileEditScreen()),
+                    text: "تعديل الصفحة الشخصية",
+                    icon: Icons.edit,
                   ),
-                  const SizedBox(
-                    height: 10,
+                  ProfileButton(
+                    press: () {},
+                    text: "تواصل معنا",
+                    icon: Icons.contact_mail_outlined,
                   ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      decoration: (BoxDecoration(
-                        color: ColorManager.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 5),
-                          )
-                        ],
-                        // border: Border.all(color: ColorManager.grayText),
-                        borderRadius: BorderRadius.circular(15),
-                      )),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("الإعــــدادات"),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(Icons.settings),
-                          ],
-                        ),
-                      ),
-                    ),
+                  ProfileButton(
+                    press: () {},
+                    text: "شروط الاستخدام",
+                    icon: Icons.policy,
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      decoration: (BoxDecoration(
-                        color: ColorManager.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 5),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(15),
-                      )),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("تسجيل خروج"),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(Icons.logout),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      decoration: (BoxDecoration(
-                        color: ColorManager.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 5),
-                          )
-                        ],
-                        // border: Border.all(color: ColorManager.grayText),
-                        borderRadius: BorderRadius.circular(15),
-                      )),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("تواصل معنا"),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(Icons.contact_mail_outlined),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      decoration: (BoxDecoration(
-                        color: ColorManager.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade200,
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 5),
-                          )
-                        ],
-                        // border: Border.all(color: ColorManager.grayText),
-                        borderRadius: BorderRadius.circular(15),
-                      )),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("شروط الاستخدام"),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Icon(Icons.policy),
-                          ],
-                        ),
-                      ),
-                    ),
+                  ProfileButton(
+                    press: () {},
+                    text: "تسجيل الخروج",
+                    icon: Icons.logout,
                   ),
                 ],
               ),
